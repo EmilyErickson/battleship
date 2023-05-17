10ecc1195993ab303e31203701ad33113bb1d5e7
EmilyErickson <emilyerickson2002@gmail.com>
2023-05-16 19:02:55 -0500

    Create basic tests

--- src/tests/ship.test.js
+++ src/tests/ship.test.js
@@ -1,19 +1,38 @@
-import { shipFactory, hit, isSunk } from "../functions";
+import { shipFactory, hit, isSunk, rotateShip } from "../ship-factory";
 
-test("shipFactory(3)", () => {
-  expect(shipFactory(3)).toStrictEqual({ length: 3, timesHit: 0, sunk: false });
+test("shipFactory(submarine, 3)", () => {
+  expect(shipFactory("submarine", 3)).toStrictEqual({
+    name: "submarine",
+    length: 3,
+    direction: "horizontal",
+    timesHit: 0,
+    sunk: false,
+  });
 });
 
 test("hit(ship) tests hits increase each time", () => {
-  let ship = shipFactory(3);
-  expect(hit(ship)).toStrictEqual({
+  let submarine = shipFactory("submarine", 3);
+  expect(hit(submarine)).toStrictEqual({
+    name: "submarine",
     length: 3,
+    direction: "horizontal",
     timesHit: 1,
     sunk: false,
   });
 });
 
 test("sunk returns true if hits = length of ship", () => {
-  let ship = hit(shipFactory(1));
-  expect(isSunk(ship)).toStrictEqual({ length: 1, timesHit: 1, sunk: true });
+  let ship = hit(shipFactory("test", 1));
+  expect(isSunk(ship)).toStrictEqual({
+    name: "test",
+    length: 1,
+    direction: "horizontal",
+    timesHit: 1,
+    sunk: true,
+  });
+});
+
+test("rotate ship", () => {
+  let ship = shipFactory("destroyer", 2);
+  expect(rotateShip(ship).direction).toBe("vertical");
 });
