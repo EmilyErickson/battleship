677b43f8fab9db55d539247bbec0a1d3f299e011
EmilyErickson <emilyerickson2002@gmail.com>
2023-07-09 13:45:40 -0500

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,32 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  watch: true,
 
   module: {
     rules: [
       {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.gif$/,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(ttf|eot|svg)$/,
+        type: "asset/resource",
       },
     ],
   },
